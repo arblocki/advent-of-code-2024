fbecbcfba8699f3296b3d237fc8c2cc6fcf79d4b
Drew Blocki <andrewrblocki@gmail.com>
2024-12-08 15:14:21 -0600

    Add placeholders for future days

---
+++ days/5/main.py
@@ -0,0 +1,14 @@
+# Advent of code Year 2024 Day {} solution
+# Author = Drew Blocki
+# Date = December 2024
+
+with open((__file__.rstrip("main.py")+"input.txt"), 'r') as input_file:
+    input = input_file.read()
+
+# PART 1
+print('PART 1')
+
+
+
+# PART 2
+# print('PART 2')
