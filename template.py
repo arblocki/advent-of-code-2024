000fda858d69587cd3e40ce16a383e5e75cf8266
Drew Blocki <andrewrblocki@gmail.com>
2024-12-07 20:05:45 -0600

    Finish first 2 days

---
+++ template.py
@@ -0,0 +1,9 @@
+# Advent of code Year 2024 Day {} solution
+# Author = Drew Blocki
+# Date = December 2024
+
+with open((__file__.rstrip("main.py")+"input.txt"), 'r') as input_file:
+    input = input_file.read()
+
+# WRITE SOLUTION HERE
+
