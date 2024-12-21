b380ff3c673a8577fce75f0ee7205a639b95b757
Drew Blocki <andrewrblocki@gmail.com>
2024-12-20 19:40:59 -0500

    Finish day 17

--- days/17/main.py
+++ days/17/main.py
@@ -17,8 +17,7 @@ bInit = int(match.group(2))
 cInit = int(match.group(3))
 program = list(map(int, match.group(4).split(',')))
 
-def comboOp(op):
-    global a, b, c
+def comboOp(a, b, c, op):
     if op == 4:
         return a
     elif op == 5:
@@ -27,25 +26,8 @@ def comboOp(op):
         return c
     return op
 
-# a = aInit
-a = 0
-b = bInit
-c = cInit
-
-# TODO: Seems like the strategy is to solve from the back, 
-# then bitshift the candidate A left 3, and continue solving
-
-aFinal = 0
-
-out = []
-outIndex = 
-while out != program:
-    a = aNext
-    b = bInit
-    c = cInit
-
+def simulateProgram(a, b, c, program):
     out = []
-    instrList = []
     index = 0
     while index < len(program):
         proceed = True
@@ -55,39 +37,43 @@ while out != program:
             break
         operand = program[index + 1]
         if instr == 0:
-            a = int(a // math.exp2(comboOp(operand)))
-            instrList.append(f'\tSETTING A TO A / 2^{comboOp(operand)} (comboOp of {operand}) : {a}')
+            a = int(a // math.exp2(comboOp(a, b, c, operand)))
         elif instr == 1:
             b = int(b ^ operand)
-            instrList.append(f'\tSETTING B TO B XOR {operand} : {b}')
         elif instr == 2:
-            b = int(comboOp(operand) % 8)
-            instrList.append(f'\tSETTING B TO {comboOp(operand)} (comboOp of {operand}) mod 8 : {b}')
+            b = int(comboOp(a, b, c, operand) % 8)
         elif instr == 3:
             if a != 0:
                 index = operand
-                instrList.append(f'\tJUMPING TO {operand}')
                 proceed = False
         elif instr == 4:
             b = int(b ^ c)
-            instrList.append(f'\tSETTING B TO B XOR C : {b}')
         elif instr == 5:
-            last = len(out)
-            out.append(int(comboOp(operand) % 8))
-            instrList.append(f'\tOUTPUTTING {comboOp(operand)} (comboOp of {operand}) mod 8 : {int(comboOp(operand) % 8)}')
-            # if out[last] != program[last]:
-            #     break
+            out.append(int(comboOp(a, b, c, operand) % 8))
         elif instr == 6:
-            b = int(a // math.exp2(comboOp(operand)))
-            instrList.append(f'\tSETTING B TO A / 2^{comboOp(operand)} (comboOp of {operand}) : {b}')
+            b = int(a // math.exp2(comboOp(a, b, c, operand)))
         elif instr == 7:
-            c = int(a // math.exp2(comboOp(operand)))
-            instrList.append(f'\tSETTING C TO A / 2^{comboOp(operand)} (comboOp of {operand}) : {c}')
+            c = int(a // math.exp2(comboOp(a, b, c, operand)))
         if proceed:
             index += 2
-    aNext <<= 3
+    return out
 
-print(','.join(map(str, out)))
-# for instr in instrList:
-#     print(instr)
-print(f'a: {aNext-1}')
+# a = aInit
+b = bInit
+c = cInit
+
+aFinal = 0
+out = []
+def solve(index, currentA):
+    if index < 0:
+        print(f'A: {currentA}')
+        return True
+    for a in range(8):
+        aTest = (currentA << 3) | a
+        out = simulateProgram(aTest, b, c, program)
+        if out == program[index:] and solve(index - 1, aTest):
+            return True
+    return False
+            
+
+solve(len(program) - 1, 0)
